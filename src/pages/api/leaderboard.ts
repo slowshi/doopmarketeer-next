9b843ace13d231966a94c7b8272532abc9538172
Len Lester <lenster921@gmail.com>
2023-04-01 15:24:36 -0700

    added back in the stuff because its not api issues

--- src/pages/api/leaderboard.ts
+++ src/pages/api/leaderboard.ts
@@ -11,51 +11,54 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
   const doopResponse = await contractTransactions(DOOPLICATOR_ADDRESS)
   const marketResponse = await contractTransactions(DOOPMARKET_ADDRESS)
   const genBoxOpenerResponse = await contractInternalTransactions(GENESIS_BOX_OPENER_ADDRESS)
-  // const genBoxResults: GenesisBoxTransactionEvent[] = genBoxOpenerResponse.result
-  // const genesisBoxTransactions: DoopTransactionInfo[] = formatGenesisBoxTransactionResponse(genBoxResults)
+  const genBoxResults: GenesisBoxTransactionEvent[] = genBoxOpenerResponse.result
+  const genesisBoxTransactions: DoopTransactionInfo[] = formatGenesisBoxTransactionResponse(genBoxResults)
 
   const doopResults: Transaction[] = doopResponse.result
   const marketResults: Transaction[] = marketResponse.result
   const transactions: DoopTransactionInfo[] = formatTransactionResponse([...doopResults, ...marketResults])
 
-  const leaderboard: LeaderboardMap = [...transactions].reduce((acc: LeaderboardMap, item: DoopTransactionInfo) => {
-    let user: LeaderboardUser = {
-      timeStamp: 0,
-      address: '',
-      dooplicate: 0,
-      genesisBox: 0,
-      dooplicateItem: 0,
-      value: 0,
-    }
-    const isDooplicate = item.functionName === 'dooplicateItem'
-    const isDooplicateItem = item.functionName === 'dooplicate'
-    const isSafeTransferFrom = item.functionName === 'safeTransferFrom'
-    if (typeof acc[item.from] === 'undefined') {
-      user = {
-        timeStamp: Number(item.timeStamp),
-        address: item.from,
-        dooplicate: isDooplicate ? 1 : 0,
-        dooplicateItem: isDooplicateItem ? 1 : 0,
-        genesisBox: isSafeTransferFrom ? 1 : 0,
-        value: Number(item.value),
+  const leaderboard: LeaderboardMap = [...transactions, ...genesisBoxTransactions].reduce(
+    (acc: LeaderboardMap, item: DoopTransactionInfo) => {
+      let user: LeaderboardUser = {
+        timeStamp: 0,
+        address: '',
+        dooplicate: 0,
+        genesisBox: 0,
+        dooplicateItem: 0,
+        value: 0,
       }
-    } else {
-      const existingUser: LeaderboardUser = acc[item.from]
-      const itemTimestamp = Number(item.timeStamp)
-      user = {
-        ...existingUser,
-        timeStamp: existingUser.timeStamp >= itemTimestamp ? existingUser.timeStamp : itemTimestamp,
-        value: Number(existingUser.value) + Number(item.value),
-        dooplicate: isDooplicate ? existingUser.dooplicate + 1 : existingUser.dooplicate,
-        dooplicateItem: isDooplicateItem ? existingUser.dooplicateItem + 1 : existingUser.dooplicateItem,
-        genesisBox: isSafeTransferFrom ? existingUser.genesisBox + 1 : existingUser.genesisBox,
+      const isDooplicate = item.functionName === 'dooplicateItem'
+      const isDooplicateItem = item.functionName === 'dooplicate'
+      const isSafeTransferFrom = item.functionName === 'safeTransferFrom'
+      if (typeof acc[item.from] === 'undefined') {
+        user = {
+          timeStamp: Number(item.timeStamp),
+          address: item.from,
+          dooplicate: isDooplicate ? 1 : 0,
+          dooplicateItem: isDooplicateItem ? 1 : 0,
+          genesisBox: isSafeTransferFrom ? 1 : 0,
+          value: Number(item.value),
+        }
+      } else {
+        const existingUser: LeaderboardUser = acc[item.from]
+        const itemTimestamp = Number(item.timeStamp)
+        user = {
+          ...existingUser,
+          timeStamp: existingUser.timeStamp >= itemTimestamp ? existingUser.timeStamp : itemTimestamp,
+          value: Number(existingUser.value) + Number(item.value),
+          dooplicate: isDooplicate ? existingUser.dooplicate + 1 : existingUser.dooplicate,
+          dooplicateItem: isDooplicateItem ? existingUser.dooplicateItem + 1 : existingUser.dooplicateItem,
+          genesisBox: isSafeTransferFrom ? existingUser.genesisBox + 1 : existingUser.genesisBox,
+        }
       }
-    }
-    acc = {
-      ...acc,
-      [item.from]: user,
-    }
-    return acc
-  }, {} as LeaderboardMap)
+      acc = {
+        ...acc,
+        [item.from]: user,
+      }
+      return acc
+    },
+    {} as LeaderboardMap,
+  )
   res.status(200).json(Object.values(leaderboard))
 }
