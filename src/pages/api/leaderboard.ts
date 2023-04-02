08a6fb6478c34500c52bad2a239317bd05a22cd6
Len Lester <lenster921@gmail.com>
2023-04-01 20:16:20 -0700

    using concat but it works

--- src/pages/api/leaderboard.ts
+++ src/pages/api/leaderboard.ts
@@ -71,26 +71,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
     }
     return acc
   }
-  const leaderboard: LeaderboardMap = transactions
+  const leaderboard: LeaderboardMap = ([] as DoopTransactionInfo[])
+    .concat(transactions)
     .concat(genesisBoxTransactions)
     .reduce(reduceTransactions, {} as LeaderboardMap)
-  // const boxLeaderboard: LeaderboardMap = [...genesisBoxTransactions].reduce(reduceTransactions, {} as LeaderboardMap)
-  // const totalLeaderboard = Object.keys(boxLeaderboard).reduce((acc: LeaderboardMap, item: string) => {
-  //   if (typeof leaderboard[item] === 'undefined') {
-  //     acc = {
-  //       ...acc,
-  //       [item]: { ...boxLeaderboard[item] },
-  //     }
-  //   } else {
-  //     acc = {
-  //       ...acc,
-  //       [item]: {
-  //         ...leaderboard[item],
-  //         genesisBox: boxLeaderboard[item].genesisBox,
-  //       },
-  //     }
-  //   }
-  //   return acc
-  // }, {} as LeaderboardMap)
   res.status(200).json(Object.values(leaderboard))
 }
